18df7d6ff93b1490d9697c6a1e36cc26c959adc8
Lorenz <19100121@usc.edu.ph>
2026-05-21 10:24:21 +0800

    added fallback

--- lib/api.ts
+++ lib/api.ts
@@ -1,4 +1,7 @@
-const DEFAULT_API_URL = "http://localhost:3001";
+const DEFAULT_API_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://villahermosadentalclinic-server.onrender.com"
+    : "http://localhost:3001";
 
 export const API_BASE_URL = (
   process.env.NEXT_PUBLIC_API_URL || DEFAULT_API_URL
